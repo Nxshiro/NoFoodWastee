dc22d3be38bf3d30cc0e3644e7f89c8163affa14
Nxshiro <anthony.mrim@gmail.com>
2022-05-14 19:20:16 +0200

    modif controller

--- NoFoodWaste/src/main/java/com/nofoodwaste/app/Register/RegisterController.java
+++ NoFoodWaste/src/main/java/com/nofoodwaste/app/Register/RegisterController.java
@@ -4,20 +4,34 @@ import com.nofoodwaste.app.User.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
+
+
 
     @PostMapping("register")
     public String register(){
 
         return "login";
     }
-    @GetMapping
+    @GetMapping("register")
     public String register(Model model){
+        model.addAttribute("userForm",new User());
+        return "register";
+    }
+
+    @PostMapping("login")
+    public String login(){
+
+        return "login";
+    }
+    @GetMapping("login")
+    public String login(Model model){
         model.addAttribute("username",new User());
         model.addAttribute("mdp",new User());
-        return "register";
+        return "login";
     }
 }
